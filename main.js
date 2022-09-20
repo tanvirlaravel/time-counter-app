ea0bd0505ad81efa92deba60371ac3f88b41ca0e
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2022-09-20 11:05:28 +0600

    show seconds, minute & hours

--- main.js
+++ main.js
@@ -7,8 +7,22 @@ const allClear = document.querySelector("#all-clear");
 
 // Global Data
 let seconds = 58;
-let minutes = 0;
-let hours = 0;
+let minutes = 2;
+let hours = 4;
 let totalSecond = 123;
 let myInterval = null;
 
+// Run program first time
+renderTime();
+
+// Functions
+
+function renderTime() {
+  showTimes();
+}
+
+function showTimes() {
+  showTime.innerText = `${hours ? hours : "00"}:${minutes ? minutes : "00"}:${
+    seconds ? seconds : "00"
+  }`;
+}
